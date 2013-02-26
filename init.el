9c728b7cdef309e35c31f75dccf0f5ae7d4a78a5
Dmitrii Rogozhin <dmn@dr-PC>
2013-02-26 15:00:34 +0400

    update default packages list

--- init.el
+++ init.el
@@ -1,9 +1,25 @@
 ;;(toggle-debug-on-error)
 (setq windows-p (string-match "mingw" (emacs-version)))
+(defun log-edit-mode () nil)
+
+;; Add mermelade
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
 ;; ElHome
+(defconst elhome-directory "~/.emacs.d/")
 (if windows-p
-    (defconst elhome-directory "h:/emacs/")
-    (defconst elhome-directory "~/.emacs.d"))
+    (progn 
+      (set-face-font 'default  "Liberation Mono")
+      ;; (set-face-font 'default  "Droid Sans Mono-8")
+      (setq my-mach-packages '())
+      (setq el-get-dir (concat (file-name-as-directory elhome-directory) "el-get/")))
+  (progn
+    (setq my-mach-packages '(magit skype vkill))
+    (defconst elhome-directory "~/.emacs.d")))
+
 (add-to-list 'load-path (concat (file-name-as-directory elhome-directory) "el-get/el-get"))
 (setq el-get-git-install-url "git://github.com/dimitri/el-get.git")
 
@@ -23,116 +39,63 @@
 ;; Local sources
 (setq el-get-sources
       '((:name initsplit
-         :description "Code to split customizations into different files"
-         :type git
-         :url "git://github.com/dabrahams/initsplit.git"
-         :features initsplit)
-	(:name bbdb
-         :type git
-         :url "git://git.savannah.nongnu.org/bbdb.git"
-         :branch "v2.x"
-         :load-path ("./lisp")
-         ;; if using vm, add `--with-vm-dir=DIR' after ./configure
-         :build `("autoconf" "rm -f lisp/bbdb-autoloads.el"
-                  ,(concat "./configure --with-emacs=" el-get-emacs) "make autoloads bbdb"))
-        (:name apel
-         :type git
-         :url "git://github.com/wanderlust/apel.git"
-         :build
-         (mapcar
-          (lambda (target)
-            (list el-get-emacs
-                  (split-string "-batch -q -no-site-file -l APEL-MK -f")
-                  target
-                  "prefix" "site-lisp" "site-lisp"))
-          '("compile-apel" "install-apel"))
-         :load-path ("site-lisp/apel" "site-lisp/emu"))
-        (:name flim
-         :depends apel
-         :type git
-         :url "git://github.com/wanderlust/flim.git"
-         :build
-         (append
-          (mapcar
-           (lambda (target)
-             (list el-get-emacs
-                   (mapcar (lambda (pkg)
-                             (mapcar (lambda (d) `("-L" ,d)) (el-get-load-path pkg)))
-                           '("apel"))
-                   (split-string "-batch -q -no-site-file -l FLIM-MK -f")
-                   target
-                   "prefix" "site-lisp" "site-lisp"))
-           '("compile-flim" "install-flim")))
-         :load-path ("site-lisp/flim"))
-        (:name semi
-         :depends (apel flim)
-         :type git
-         :url "git://github.com/wanderlust/semi.git"
-         :build
-         (append
-          (mapcar
-           (lambda (target)
-             (list el-get-emacs
-                   (mapcar (lambda (pkg)
-                             (mapcar (lambda (d) `("-L" ,d)) (el-get-load-path pkg)))
-                           '("apel" "flim"))
-                   (split-string "-batch -q -no-site-file -l SEMI-MK -f")
-                   target
-                   "prefix" "site-lisp" "site-lisp"))
-           '("compile-semi" "install-semi")))
-         :load-path ("site-lisp/semi/"))
-        (:name wanderlust
-         :depends semi
-         :type git
-         :url "git://github.com/wanderlust/wanderlust.git"
-         :build (mapcar
-                 (lambda (target-and-dirs)
-                   (list el-get-emacs
-                         (mapcar (lambda (pkg)
-                                   (mapcar (lambda (d) `("-L" ,d)) (el-get-load-path pkg)))
-                                 (append
-                                  '("apel" "flim" "semi")
-                                  (when (el-get-package-exists-p "bbdb") (list "bbdb"))))
-                         "--eval" (prin1-to-string
-                                   '(progn (setq wl-install-utils t)
-                                     (setq wl-info-lang "en")
-                                     (setq wl-news-lang "en")))
-                         (split-string "-batch -q -no-site-file -l WL-MK -f")
-                         target-and-dirs))
-                 '(("wl-texinfo-format" "doc")
-                   ("compile-wl-package"  "site-lisp" "icons")
-                   ("install-wl-package" "site-lisp" "icons")))
-         :info "doc/wl.info"
-         :load-path ("site-lisp/wl" "utils"))
+               :description "Code to split customizations into different files"
+               :type git
+               :url "git://github.com/dabrahams/initsplit.git"
+               :features initsplit)
+        (:name dash
+               :type elpa
+               :description "Dash"
+               :features dash)
         (:name ps-ccrypt
-         :type http
-         :url "http://ccrypt.sourceforge.net/ps-ccrypt.el"
-         :post-init (require 'ps-ccrypt)
-         :features ps-ccrypt)
-	(:name igrep
-         :type emacsmirror)
-	(:name kill-ring-search
-	       :website "http://nschum.de/src/emacs/kill-ring-search/"
-	       :description "Search the kill ring incrementally and yank the result"
-	       :type elpa
-	       :url "kill-ring-search")
+               :type http
+               :url "http://ccrypt.sourceforge.net/ps-ccrypt.el"
+               :post-init (require 'ps-ccrypt)
+               :features ps-ccrypt)
+        (:name igrep
+               :type emacsmirror)
+        (:name kill-ring-search
+               :website "http://nschum.de/src/emacs/kill-ring-search/"
+               :description "Search the kill ring incrementally and yank the result"
+               :type elpa
+               :url "kill-ring-search")
         (:name color-theme-less :type http :url "http://jblevins.org/git/misc.git/plain/color-theme-less.el"
                :depends color-theme
-               :after (progn (require 'color-theme-less) (color-theme-less)))
-        (:name typing
-               :features typing
-               :after (progn (require 'typing)))
-        (:name magit :after (progn (global-set-key (kbd "C-x C-m") 'magit-status)))))
+               :post-init (progn (require 'color-theme-less) (color-theme-less)))
+        ;; (:name typing
+        ;;        :features typing
+        ;;        :after (progn (require 'typing)))
+        ))
 
 (setq my-packages
-      (append
-       '(el-get elhome ergoemacs-keybindings emacs-jabber vkill speck csv-mode
-         eval-sexp-fu rainbow-delimiters paredit paredit-extension parenthesis parenface
-         highlight highlight-parentheses highlight-sexp highlight-symbol smex
-         skype js2-mode color-theme rainbow-mode mark-multiple kill-ring-search smart-tab
-	 auto-complete ac-slime ac-dabbrev volatile-highlights 
-         )
-       (mapcar 'el-get-source-name el-get-sources)))
+      (append my-mach-packages
+              '(elhome
+                ergoemacs-keybindings
+                geiser
+                ;; speck csv-mode eval-sexp-fu
+                rainbow-delimiters
+                paredit
+                ;; paredit-extension parenthesis
+                parenface
+                ;; highlight
+                highlight-parentheses
+                highlight-indentation
+                volatile-highlights
+                ;; highlight-sexp highlight-symbol smex
+                js2-mode
+                js2-refactor
+                js2-highlight-vars
+                color-theme
+                rainbow-mode
+                mark-multiple
+                kill-ring-search
+                smart-tab
+                scss-mode
+                jade-mode
+                auto-complete
+                ac-slime
+                ac-dabbrev)
+              (mapcar 'el-get-source-name el-get-sources)))
 (add-hook 'el-get-post-install-hooks 'el-get-init)
 ;; you should be connected to Net at this point
 (el-get 'sync my-packages)
