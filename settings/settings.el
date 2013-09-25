cdd649235e9c0d21ab3118bb08d906984b3bb59b
Demyan Rogozhin <demyan.rogozhin@gmail.com>
2013-09-25 12:55:46 +0400

    custom-file indented

--- settings/settings.el
+++ settings/settings.el
@@ -8,7 +8,17 @@
  '(bbdb-auto-notes-ignore (quote (("From" . "noreply") ("From" . "no-reply"))))
  '(bbdb-auto-notes-ignore-all (quote (("From" . "portal-noreply") ("From" . "facebook"))))
  '(bbdb-file "~/Mail/bbdb")
- '(bbdb-ignore-some-messages-alist (quote (("From" . "no-reply") ("From" . "portal-noreply") ("From" . "noreply") ("From" . "no_reply") ("ebay" . "") ("To" . "debian") ("To" . "enlightenment") ("Cc" . "list") ("Xref" . "gmane"))))
+ '(bbdb-ignore-some-messages-alist
+   (quote
+    (("From" . "no-reply")
+     ("From" . "portal-noreply")
+     ("From" . "noreply")
+     ("From" . "no_reply")
+     ("ebay" . "")
+     ("To" . "debian")
+     ("To" . "enlightenment")
+     ("Cc" . "list")
+     ("Xref" . "gmane"))))
  '(bbdb-new-nets-always-primary (quote never))
  '(bbdb-north-american-phone-numbers-p nil)
  '(bbdb-notice-hook (quote (bbdb-auto-notes-hook)))
@@ -16,26 +26,40 @@
  '(bbdb/gnus-summary-mark-known-posters t)
  '(bbdb/mail-auto-create-p (quote bbdb-ignore-some-messages-hook))
  '(bbdb/news-auto-create-p nil)
- '(comint-mode-hook (quote (ergoemacs-comint-hook ansi-color-for-comint-mode-on)))
+ '(comint-mode-hook
+   (quote
+    (ergoemacs-comint-hook ansi-color-for-comint-mode-on)))
  '(company-idle-delay 0.2)
  '(company-minimum-prefix-length 2)
  '(company-tooltip-minimum 3)
  '(css-indent-offset 2)
  '(custom-enabled-themes (quote (system-type-windows-nt)))
- '(custom-safe-themes (quote ("c52e4cc847e73153c1c4fd2f6e528ff9f7b560e203b37d0b6c749161e36da843" "37ce6638dc23a612a861357862f676041ad7b6547ccafa0c6249c1190f127906" default)))
+ '(custom-safe-themes
+   (quote
+    ("c52e4cc847e73153c1c4fd2f6e528ff9f7b560e203b37d0b6c749161e36da843" "37ce6638dc23a612a861357862f676041ad7b6547ccafa0c6249c1190f127906" default)))
  '(ede-project-directories (quote ("/home/dmn/hanoi" "/home/dmn/tmp/ede")))
  '(el-get-github-default-url-type (quote https))
- '(el-get-recipe-path (quote ("~/.emacs.d/el-get/el-get/recipes" "~/.emacs.d/el-get/el-get/recipes/emacswiki/" "~/.emacs.d/site-lisp/recipes/")))
+ '(el-get-recipe-path
+   (quote
+    ("~/.emacs.d/el-get/el-get/recipes" "~/.emacs.d/el-get/el-get/recipes/emacswiki/" "~/.emacs.d/site-lisp/recipes/")) t)
  '(highlight-indentation-offset 2)
  '(highlight-symbol-idle-delay 0.5)
- '(hl-paren-colors (quote ("DodgerBlue1" "RoyalBlue2" "RoyalBlue3" "RoyalBlue4")))
+ '(hl-paren-colors
+   (quote
+    ("DodgerBlue1" "RoyalBlue2" "RoyalBlue3" "RoyalBlue4")))
  '(hl-sexp-background-color "#ffffff")
  '(hl-sexp-face (quote highlight))
  '(indent-tabs-mode nil)
  '(indicate-buffer-boundaries (quote left))
  '(indicate-empty-lines t)
  '(inhibit-startup-screen t)
- '(initial-frame-alist (quote ((vertical-scroll-bars) (tool-bar-lines . 0) (menu-bar-lines . 0) (left-fringe . 3) (right-fringe . 4))))
+ '(initial-frame-alist
+   (quote
+    ((vertical-scroll-bars)
+     (tool-bar-lines . 0)
+     (menu-bar-lines . 0)
+     (left-fringe . 3)
+     (right-fringe . 4))))
  '(initsplit-pretty-print nil)
  '(js2-allow-keywords-as-property-names nil)
  '(js2-auto-indent-p t)
@@ -49,7 +73,52 @@
  '(menu-bar-mode nil)
  '(mmm-submode-decoration-level 0)
  '(pr-gs-command "gswin64.exe")
- '(safe-local-variable-values (quote ((eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook" (add-hook (quote write-contents-functions) (lambda nil (delete-trailing-whitespace) nil)) (require (quote whitespace)) "Sometimes the mode needs to be toggled off and on." (whitespace-mode 0) (whitespace-mode 1)) (js-run . "swank-handler-tests.js") (lexical-binding . t) (js-run . "lisp-tests.js") (eval whitespace-mode) (eval set-face-attribute (quote whitespace-line) nil :background "red1" :foreground "yellow" :weight (quote bold)) (eval set-face-attribute (quote whitespace-tab) nil :background "red1" :foreground "yellow" :weight (quote bold)) (eval require (quote whitespace)) (eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook" (add-hook (quote write-contents-functions) (lambda nil (delete-trailing-whitespace (point-min) (point-max)) nil)) (require (quote whitespace)) "Sometimes the mode needs to be toggled off and on." (whitespace-mode 0) (whitespace-mode 1)) (whitespace-line-column . 80) (whitespace-style face trailing lines-tail) (require-final-newline . t) (eval add-hook (quote write-file-hooks) (quote time-stamp)))))
+ '(safe-local-variable-values
+   (quote
+    ((eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook"
+           (add-hook
+            (quote write-contents-functions)
+            (lambda nil
+              (delete-trailing-whitespace)
+              nil))
+           (require
+            (quote whitespace))
+           "Sometimes the mode needs to be toggled off and on."
+           (whitespace-mode 0)
+           (whitespace-mode 1))
+     (js-run . "swank-handler-tests.js")
+     (lexical-binding . t)
+     (js-run . "lisp-tests.js")
+     (eval whitespace-mode)
+     (eval set-face-attribute
+           (quote whitespace-line)
+           nil :background "red1" :foreground "yellow" :weight
+           (quote bold))
+     (eval set-face-attribute
+           (quote whitespace-tab)
+           nil :background "red1" :foreground "yellow" :weight
+           (quote bold))
+     (eval require
+           (quote whitespace))
+     (eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook"
+           (add-hook
+            (quote write-contents-functions)
+            (lambda nil
+              (delete-trailing-whitespace
+               (point-min)
+               (point-max))
+              nil))
+           (require
+            (quote whitespace))
+           "Sometimes the mode needs to be toggled off and on."
+           (whitespace-mode 0)
+           (whitespace-mode 1))
+     (whitespace-line-column . 80)
+     (whitespace-style face trailing lines-tail)
+     (require-final-newline . t)
+     (eval add-hook
+           (quote write-file-hooks)
+           (quote time-stamp)))))
  '(scroll-bar-mode nil)
  '(scss-compile-at-save nil)
  '(scss-sass-options (quote ("-t" "expanded")))
